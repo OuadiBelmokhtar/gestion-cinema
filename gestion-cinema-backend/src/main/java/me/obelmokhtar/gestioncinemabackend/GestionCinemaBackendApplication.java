134d3535acbd21b1e5dcdf212e894aa31102c3c9
ouadi.belmokhtar <ouadi.belmokhtar@gmail.com>
2022-09-08 17:43:33 +0100

    création du projet backend vide

---
+++ gestion-cinema-backend/src/main/java/me/obelmokhtar/gestioncinemabackend/GestionCinemaBackendApplication.java
@@ -0,0 +1,13 @@
+package me.obelmokhtar.gestioncinemabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCinemaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionCinemaBackendApplication.class, args);
+    }
+
+}
---
+++ gestion-cinema-backend/src/main/java/me/obelmokhtar/gestioncinemabackend/entities/Film.java
@@ -0,0 +1,27 @@
+package me.obelmokhtar.gestioncinemabackend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titre;
+    private Integer duree;
+    private String realisateur;
+    private String description;
+    private String photo;
+    private Date dateSortie;
+}
